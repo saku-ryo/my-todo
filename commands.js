11d78134ae3c156ffba83a766261adb259ea603a
S307685-001\sansou <265216240+saku-ryo@users.noreply.github.com>
2026-04-02 16:22:37 +0900

    インデント等の修正 #6

--- commands.js
+++ commands.js
@@ -8,28 +8,34 @@ function addTodos(task) {
 
     if(!task) return console.log("追加するタスクを入力してください");
 
-        // ファイルを読込み
-        const todos = fileManager.loadTodos();
+    // ファイルを読込み
+    const todos = fileManager.loadTodos();
 
-        if (todos === undefined)  {
-            console.log("JSONファイルが存在しません");
-            return
-        }
-       // idの生成
-        const id = uuidv4();
-        // 日付のフォーマット
-        const day = dayjs().format('YYYY-MM-DD HH:mm');
-        // 未完了状態をセット
-        const status = "未完了";
-        // タスクを追加する情報をセット
-        todos.push({ id, task, day, status});
-        // タスクの情報をJSONファイルに保存
-        fileManager.saveTodos(todos)
-        console.log(chalk.green('タスクを追加しました！'));
+    if (todos === undefined)  {
+        console.log("JSONファイルが存在しません");
+        return
+    }
+    
+    // idの生成
+    const id = uuidv4();
+    
+    // 日付のフォーマット
+    const day = dayjs().format('YYYY-MM-DD HH:mm');
+    
+    // 未完了状態をセット
+    const status = "未完了";
+    
+    // タスクを追加する情報をセット
+    todos.push({ id, task, day, status});
+    
+    // タスクの情報をJSONファイルに保存
+    fileManager.saveTodos(todos)
+    console.log(chalk.green('タスクを追加しました！'));
 }
 
 // list表示
 function listTodos() {
+
     // JSONファイルを読込み
     const todos = fileManager.loadTodos();
 
@@ -64,7 +70,6 @@ function doneTodos(id) {
     todo.status = "完了"
     fileManager.saveTodos(todos);
     console.log(chalk.green("ステータスを更新しました"));
-
 }
 
 // 削除コマンド処理
@@ -72,18 +77,17 @@ function deleteTodos(id) {
 
     // JSONファイルを読込み
     const todos = fileManager.loadTodos();
+
     // 引数で入力されたIDを検索し、データを取得
     const delTodo = todos.filter(t => t.id !== id);
-
 
     if(todos.length === delTodo.length) {
         console.log(chalk.red("指定されたIDは存在しません。"));
         return
     }
-    //　データの保存
+    // データの保存
     fileManager.saveTodos(delTodo);
     console.log(chalk.yellow("指定されたIDのデータを削除しました。"));
-
 }
 
 module.exports = {addTodos, listTodos, doneTodos, deleteTodos};
--- todo.js
+++ todo.js
@@ -8,23 +8,21 @@ if(!fs.existsSync(fileManager.FILE)) {
     fs.writeFileSync(fileManager.FILE,JSON.stringify([]));
 }
 
-//commanderの定義
+// commanderの定義
 program
     .version('1.0.0', '-v, --version')
     .description('TODO管理ツール');
 
-//　コマンド判別
+// コマンド判別
 // 引数：add<task>の処理
 program
     .command('add <task>')
     .action(addTodos);
-    
 
 // 引数：listの処理
 program
     .command('list')
     .action(listTodos);
-    
 
 // 引数：done<id>の処理
 program
@@ -36,5 +34,5 @@ program
     .command('delete <id>')
     .action(deleteTodos);
 
-//引数の解析
+// 引数の解析
 program.parse(process.argv);
